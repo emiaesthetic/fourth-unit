c1d75cb2576ced1220849f5fa35a4e0a8a14f98a
emiaesthetic <emi.aesthetic@gmail.com>
2024-04-24 10:48:27 +0500

    Lesson-01: fundamentals of programming and JS

---
+++ js/task.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productTitle = 'Навигационная система Soundmax';
+const productQuantity = 5;
+const productCategory = 'Техника для дома';
+const productPrice = 100;
+
+console.log(productTitle);
+
+const productTotalPrice = productPrice * productQuantity;
+
+console.log(productTotalPrice);
